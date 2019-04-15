c45bac9c54658e3ab122241a4bb10b4011018455
Justas Palumickas <jpalumickas@gmail.com>
2019-04-15 23:00:28 +0300

    Export Box as default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import MiddlewaresProvider from './Provider'
 import { Consumer as MiddlewaresConsumer } from './Context'
+import Box from './Box';
+
+export default Box;
 
 export {
   MiddlewaresProvider,

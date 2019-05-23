a19e007300eeb79c867cee10694710d5a2659069
Justas Palumickas <jpalumickas@gmail.com>
2019-05-23 20:54:17 +0300

    Fix global css early return

--- src/applyMiddlewares.js
+++ src/applyMiddlewares.js
@@ -23,16 +23,18 @@ const isMiddlewareMatch = (match, key, value) => {
 const injectGlobalCss = (css, global = true) => {
   const id = `amendable-${hashStr(css)}`;
   const stylisSelector = global ? '' : `.${id}`;
+  const className = global ? null : id;
 
-  if (document.head.querySelector(`#${id}`)) return {};
+  if (!document.head.querySelector(`#${id}`)) {
+    const node = document.createElement('style')
+    node.id = id
+    node.textContent = stylis(stylisSelector, css)
+    node.type = 'text/css'
 
-  const node = document.createElement('style')
-  node.id = id
-  node.textContent = stylis(stylisSelector, css)
-  node.type = 'text/css'
+    document.head.appendChild(node)
+  }
 
-  document.head.appendChild(node)
-  return { className: global ? null : id }
+  return { className }
 }
 
 const applyMiddlewares = ({ middlewares, ...contextRest }, props) => {

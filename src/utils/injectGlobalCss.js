a892c8a0d0d4d6b26a69805e4e483f60614d3b11
Justas Palumickas <jpalumickas@gmail.com>
2019-11-06 15:19:06 +0200

    Faster checking existing css ids

--- src/utils/injectGlobalCss.js
+++ src/utils/injectGlobalCss.js
@@ -1,18 +1,21 @@
 import hash from '@amendable/hash';
 import stylis from 'stylis';
 
+const existingIds = [];
+
 export default (css, global = true) => {
   const id = `amendable-${hash(css)}`;
   const stylisSelector = global ? '' : `.${id}`;
   const className = global ? null : id;
 
-  if (!document.head.querySelector(`#${id}`)) {
+  if (!existingIds.includes(id) && !document.head.querySelector(`#${id}`)) {
     const node = document.createElement('style')
     node.id = id
     node.textContent = stylis(stylisSelector, css)
     node.type = 'text/css'
 
     document.head.appendChild(node)
+    existingIds.push(id);
   }
 
   return { className }

d57e7519459eb207eba6bf18aaff941249f71e31
Justas Palumickas <jpalumickas@gmail.com>
2019-11-26 22:21:25 +0200

    Some more fixes

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
 export interface Resolver {
   match: any,
+  css?: ({}) => string | string,
+  globalCss?: ({}) => string | string,
+  resolve?: ({}) => object,
+  options?: ({}: { key: string, value: any }) => object,
 }
 
 export interface ProviderProps {
-  resolvers?: Resolver[]
+  resolvers?: Resolver[],
 }

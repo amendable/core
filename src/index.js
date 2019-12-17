46b4e7cc2b664eca4f557be8453993d07045035d
Justas Palumickas <jpalumickas@gmail.com>
2019-12-17 11:14:14 +0200

    Export Box from named

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export const useAmendable = use(Context);
 
 export { default as AmendableProvider } from './Provider';
 export { Consumer as AmendableConsumer } from './Context';
+export { default as Box } from './Box';
 export { default } from './Box';
